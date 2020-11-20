889b9047a5677b285420b7600421fa6978e1dcc0
sjuush <74620010+sjuush@users.noreply.github.com>
2020-11-20 13:11:17 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,2 +1,6 @@
 x = input("What is your favourite colour?")
 print("{} !, That's my favourite colour as well!".format(x))
+
+x = int(input("How many cans come in a pack?"))
+y = int(input("How many packs are there?"))
+print("The amount of cans is:", x*y)
