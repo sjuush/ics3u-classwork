01da1e96329e04c7185b01650c06332d4256bb35
sjuush <74620010+sjuush@users.noreply.github.com>
2020-11-20 13:16:15 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -4,3 +4,8 @@ print("{} !, That's my favourite colour as well!".format(x))
 x = int(input("How many cans come in a pack?"))
 y = int(input("How many packs are there?"))
 print("The amount of cans is:", x*y)
+
+x = int(input("What is the length?"))
+y = int(input("What is the width?"))
+z = int(input("What is the height?"))
+print("The volume of the rectangular prism is:", x * y * z)
