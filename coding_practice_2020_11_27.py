081f4b2ae97e5681a737d641727faae5141b1ab6
sjuush <74620010+sjuush@users.noreply.github.com>
2020-11-27 14:14:05 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,64 @@
+1. 
+i = 448
+while i < 536:
+  print(i)
+  i +=2
+
+2. 
+i = 459
+while i < 561:
+  print(i)
+  i += 3
+
+3. 
+i = -47
+while i < 281:
+  print(i)
+  i += 8
+
+4. 
+i = -94
+while i < -94:
+  print(i)
+  i += 0
+
+5. 
+i = -327
+while i < -82:
+  print(i)
+  i += 7
+
+6. 
+i = 117
+while i < 337:
+  print(i)
+  i += 11
+
+7. 
+i = -44
+while i < -4:
+  print(i)
+  i += 1
+
+8. 
+i = 104
+while i < 824:
+  print(i)
+  i += 18
+
+9. 
+i = 420
+while i < 932:
+  print(i)
+  i += 16
+
+10.
+i = -293
+while i < 139:
+  print(i)
+  i += 12
+
+
+
+
+
